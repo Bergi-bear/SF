917972d9733ce963f5fb50be390344027b1feeab
Beegi-bear <tarantyl@inbox.ru>
2020-01-10 21:59:16 +0300

    Cheapack

---
+++ build.lua
@@ -0,0 +1,12 @@
+local pack = require 'cheapack'
+
+pack.build(
+		[[D:\Games\Blizzard\Warcraft III Beta\x86_64]],
+		[[C:\Users\Bergi\IdeaProjects\SpellFighters]],
+		'map.w3x',
+		{
+			'src',
+
+		},
+		'game1'
+)
