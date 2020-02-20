02f1be14462647b10410418b9c93993fe44125b9
Beegi-bear <tarantyl@inbox.ru>
2020-02-20 21:56:49 +0300

    Починил

--- map.w3x/war3map.lua
+++ map.w3x/war3map.lua
@@ -1254,6 +1254,7 @@ function InitUnitDeath()
 	TriggerRegisterAnyUnitEventBJ(gg_trg_DEADGUI, EVENT_PLAYER_UNIT_DEATH)--меня полностью устраивает это событие
 	TriggerAddAction(gg_trg_DEADGUI, function()
 		local deadunit=GetTriggerUnit()--умерший
+		local caster=deadunit
 		local killer=GetKillingUnit()-- его убийца
 		if killer==nil then killer=deadunit end --  сам себя
 
--- src/UnitDead.lua
+++ src/UnitDead.lua
@@ -8,18 +8,22 @@ function InitUnitDeath()
 	TriggerRegisterAnyUnitEventBJ(gg_trg_DEADGUI, EVENT_PLAYER_UNIT_DEATH)--меня полностью устраивает это событие
 	TriggerAddAction(gg_trg_DEADGUI, function()
 		local deadunit=GetTriggerUnit()--умерший
+		local caster=deadunit
 		local killer=GetKillingUnit()-- его убийца
 		if killer==nil then killer=deadunit end --  сам себя
 
 		if GetUnitTypeId(deadunit)==FourCC('n012') then -- смерть великого Духа воды, рождение средних
 			CreateUnitSimpleEffect(caster,FourCC('n012'),"Abilities/Spells/Other/Silence/SilenceAreaBirth.mdl")
-		--fixme не работает призыв после смерти
 			
-		--todo пример тригера когда юнит убийца(любого юнита) получает бонусы(например хп)
+
 		elseif GetUnitTypeId(deadunit)==FourCC('n001')  then -- мелкий волк
-
 		elseif GetUnitTypeId(deadunit)==FourCC('n003') then -- большой волк
-
 		end
+		--Новое
+		--todo пример тригера когда юнит убийца(любого юнита) получает бонусы(например хп)
+		if GetUnitTypeId(killer)==FourCC('n014') then -- получаем атрибуты, увеличиваем максимальное хп
+			UnitAddBonus(killer,6,50)
+		end
+		
 	end)
 end
